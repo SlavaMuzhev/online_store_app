7b0247ce65eefb23e3551d9f51f5abd1e4b6958a
Slava Muzhev <mzvslv@gmail.com>
2026-03-26 19:55:45 +0500

    add tests/test.smartphone.py tests/lawngrass.py

---
+++ tests/test_lawngrass.py
@@ -0,0 +1,20 @@
+import pytest
+from src.lawngrass import LawnGrass
+from src.product import Product
+
+
+def test_lawngrass_init(grass_green):
+    """Проверка инициализации специфических атрибутов травы"""
+    assert grass_green.name == "Газон"
+    assert grass_green.country == "Россия"
+    assert grass_green.germination_period == "10 дней"
+    assert grass_green.color == "Зеленый"
+
+def test_lawngrass_add(grass_green, grass_sport):
+    """Проверка сложения двух объектов травы"""
+    assert grass_green + grass_sport == 17000.0
+
+def test_lawngrass_type_error(grass_green):
+    """Проверка ошибки при сложении травы с чем-то другим"""
+    with pytest.raises(TypeError):
+        _ = grass_green + 100
